894cb0f1c933c129c37d6a9523eacf17ae52bf56
ifiokInyang <ifiok.inyang@decagon.dev>
2023-06-02 21:26:14 +0100

    added useful message for pages under construction

--- src/component/About/About.tsx
+++ src/component/About/About.tsx
@@ -1,7 +1,9 @@
 import React from "react";
 
 const About = () => {
-	return <div></div>;
+	return <div className="text-[30px] mb-40">
+		This page is under construction
+	</div>;
 };
 
 export default About;
--- src/component/Contact/Contacts.tsx
+++ src/component/Contact/Contacts.tsx
@@ -1,7 +1,9 @@
 import React from "react";
 
 const Contacts = () => {
-	return <div></div>;
+	return (
+    <div className="text-[30px] mb-40">This page is under construction</div>
+  );;
 };
 
 export default Contacts;
