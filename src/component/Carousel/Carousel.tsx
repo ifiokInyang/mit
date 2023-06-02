e9d49aa462e1ce5b50d6f7b23f13775591cec97d
ifiokInyang <ifiok.inyang@decagon.dev>
2023-06-02 20:04:12 +0100

    centered quote button

--- src/component/Carousel/Carousel.tsx
+++ src/component/Carousel/Carousel.tsx
@@ -25,7 +25,7 @@ const Carousel = () => {
 				</motion.h4>
 				<button
 					type="button"
-					className="bg-gradient-to-r from-[#52462d] to-blue-900 h-[50px] w-[250px] sm:hidden md:block sm:my-4 ss:my-2 ml-4 hover:to-[#f2bf4b] hover:from-blue-700 transition duration-500 delay-150 hover:scale-110 hover:-translate-y-1 ease-in-out text-white font-semibold py-2 px-4 rounded"
+					className="bg-gradient-to-r from-[#52462d] to-blue-900 h-[50px] w-[250px] sm:hidden md:block sm:my-4 ss:my-2 sm:ml-4 ss:ml-0 hover:to-[#f2bf4b] hover:from-blue-700 transition duration-500 delay-150 hover:scale-110 hover:-translate-y-1 ease-in-out text-white font-semibold py-2 px-4 rounded"
 				>
 					Get an instant quote!
 				</button>
