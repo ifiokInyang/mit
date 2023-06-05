62961ea720d7b74d77c56b32d73aca73fb27fa1a
ifiokInyang <ifiok.inyang@decagon.dev>
2023-06-05 09:50:09 +0100

    worked on chat integrations

--- src/component/Chat/Chat.tsx
+++ src/component/Chat/Chat.tsx
@@ -1,17 +1,23 @@
 import React from "react";
+import "./Chat.css";
 
 const Chat = () => {
 	return (
-		<div className="fixed bottom-5 right-5">
+		<div className="fixed bottom-5 left-3">
 			<div className="flex justify-end">
 				<a
 					href="https://api.whatsapp.com/send/?phone=2348038384141&text&type=phone_number&app_absent=0"
 					target="_blank"
-					className="bg-[#25D366] px-4 py-2 flex items-center text-white md:text-[24px] rounded-full relative"
+					// className="bg-[#25D366] px-4 py-2 flex items-center text-white md:text-[24px] rounded-full relative"
 					rel="noreferrer"
 				>
-					Start live chat!!
-					<div className="md:w-[120px] ss:w-[80px] h-[30px] absolute top-2 left-9 border-2 border-green-800 rounded-full animate-ping"></div>
+					<div className="white">
+						<div className="socialContainer">
+							<i className="fas fa-phone"></i>
+						</div>
+					</div>
+					{/* Start live chat!! */}
+					<div className="md:w-[60px] ss:w-[80px] h-[60px] absolute top-2 left-4 border-2 border-green-800 rounded-full animate-ping"></div>
 				</a>
 			</div>
 		</div>
